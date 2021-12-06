9f0e6e6cb2be52c22f359a1ce547b49d95ad7c78
lzlaa <liuzhilei@qiniu.com>
2021-12-06 17:46:52 +0800

    task 1

--- kernel/exec.c
+++ kernel/exec.c
@@ -9,6 +9,29 @@
 
 static int loadseg(pde_t *pgdir, uint64 addr, struct inode *ip, uint offset, uint sz);
 
+void iPrintf(int i) {
+  for (;i > 0;i--) printf(".. ");
+  printf("..");
+}
+
+// print page table recursivly.
+void rPrintf(pagetable_t curPt, int depth) {
+  if (depth > 2) return;
+  for (int i = 0; i < 512; i++) {
+    pte_t* pte = curPt + i;
+    if ((*pte & PTE_V) == 0) continue;
+    iPrintf(depth);
+    printf("%d: pte %p pa %p\n", i, *pte, PTE2PA(*pte));
+    rPrintf((pde_t*)PTE2PA(*pte), depth + 1);
+  }
+}
+
+void vmprint(pagetable_t pt)
+{
+  printf("page table %p\n", pt);
+  rPrintf(pt, 0);
+}
+
 int
 exec(char *path, char **argv)
 {
@@ -115,7 +138,7 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
-
+  vmprint(p->pagetable);
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
